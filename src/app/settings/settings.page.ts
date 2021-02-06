ba797254ee17081bc77824203d18bea3fc16b232
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2021-02-06 21:34:36 +0100

    Add darkMode toggle in settings displaying preferred color scheme

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['settings.page.scss']
 })
 export class SettingsPage {
+  public settingsForm: FormGroup;
+  public title = 'Settings';
+  public darkModeEnabled = this.isDarkModePreferred().matches;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit() {
+    this.settingsForm = this.formBuilder.group({
+      darkMode: [this.darkModeEnabled]
+    });
+
+    this.isDarkModePreferred()
+      .addEventListener('change', (event) => {
+        return this.checkToggle(event.matches);
+      });
+  }
+
+  checkToggle(shouldCheck: boolean) {
+    this.settingsForm.get('darkMode').setValue(shouldCheck);
+  }
+
+  isDarkModePreferred(): MediaQueryList {
+    return window.matchMedia('(prefers-color-scheme: dark)');
+  }
 }
