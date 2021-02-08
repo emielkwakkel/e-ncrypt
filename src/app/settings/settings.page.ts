011419715a63f3990df2ccfac5c068836d11f6b8
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2021-02-08 23:31:21 +0100

    Unsubscribe subscriptions on destroy

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { EncryptionAlgorithmOptions, HashingAlgorithmOptions, HashingAlgorithms } from '../crypto.service';
 import { SettingsService } from './settings.service';
 
@@ -7,7 +8,7 @@ import { SettingsService } from './settings.service';
   selector: 'app-settings',
   templateUrl: 'settings.page.html',
 })
-export class SettingsPage implements OnInit {
+export class SettingsPage implements OnInit, OnDestroy {
   public encryptionAlgorithmOptions: any = {
     header: 'Select encryption algorithm',
   };
@@ -16,6 +17,7 @@ export class SettingsPage implements OnInit {
   };
   public hashingAlgorithms = HashingAlgorithms;
   public settingsForm: FormGroup;
+  public subscriptions: Subscription[];
   public title = 'Settings';
 
   constructor(
@@ -31,27 +33,34 @@ export class SettingsPage implements OnInit {
       hashingAlgorithm: [this.settingsService.encryptionAlgorithm],
       hashingRounds: [this.settingsService.hashingRounds.value],
     });
-    this.settingsForm.controls.encryptionAlgorithm.valueChanges.subscribe((selectedValue: EncryptionAlgorithmOptions) => {
-      this.settingsService.encryptionAlgorithm.next(selectedValue);
-    });
-    this.settingsForm.controls.hashingAlgorithm.valueChanges.subscribe((selectedValue: HashingAlgorithmOptions) => {
-      this.settingsService.hashingAlgorithm.next(selectedValue);
-    });
-    this.settingsForm.controls.darkMode.valueChanges.subscribe((selectedValue: boolean) => {
-      this.settingsService.setDarkModeEnabled(selectedValue);
-    });
-    this.settingsForm.controls.encryptionRounds.valueChanges.subscribe((selectedValue: number) => {
-      this.settingsService.encryptionRounds.next(selectedValue);
-    });
-    this.settingsForm.controls.hashingRounds.valueChanges.subscribe((selectedValue: number) => {
-      this.settingsService.hashingRounds.next(selectedValue);
-    });
+
+    this.subscriptions = [
+      this.settingsForm.controls.encryptionAlgorithm.valueChanges.subscribe((selectedValue: EncryptionAlgorithmOptions) => {
+        this.settingsService.encryptionAlgorithm.next(selectedValue);
+      }),
+      this.settingsForm.controls.hashingAlgorithm.valueChanges.subscribe((selectedValue: HashingAlgorithmOptions) => {
+        this.settingsService.hashingAlgorithm.next(selectedValue);
+      }),
+      this.settingsForm.controls.darkMode.valueChanges.subscribe((selectedValue: boolean) => {
+        this.settingsService.setDarkModeEnabled(selectedValue);
+      }),
+      this.settingsForm.controls.encryptionRounds.valueChanges.subscribe((selectedValue: number) => {
+        this.settingsService.encryptionRounds.next(selectedValue);
+      }),
+      this.settingsForm.controls.hashingRounds.valueChanges.subscribe((selectedValue: number) => {
+        this.settingsService.hashingRounds.next(selectedValue);
+      }),
+    ]
 
     // Check if dark mode preference on device changes
     this.settingsService.isDarkModePreferred()
       .addEventListener('change', (event) => {
         return this.checkDarkModeToggle(event.matches);
       });
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
   checkDarkModeToggle(shouldCheck: boolean) {
