ebb745462282b7b0ec381b6d405d30001ef8418e
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2021-02-24 23:07:57 +0100

    Fix setup settings page test

--- src/app/settings/settings.page.spec.ts
+++ src/app/settings/settings.page.spec.ts
@@ -1,16 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { SettingsPage } from './settings.page';
+import { SettingsService } from './settings.service';
 
-describe('SettingsPage', () => {
+fdescribe('SettingsPage', () => {
   let component: SettingsPage;
   let fixture: ComponentFixture<SettingsPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SettingsPage ],
-      imports: [ IonicModule.forRoot() ]
+      imports: [
+        IonicModule.forRoot(),
+        ReactiveFormsModule,
+        RouterTestingModule,
+      ],
+      providers: [
+        SettingsService,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SettingsPage);
