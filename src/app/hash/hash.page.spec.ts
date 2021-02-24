bbc22501100ae8a64e5eab1a16d0aefa79f89cff
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2021-02-24 22:36:08 +0100

    Fix basic hash page test setup

--- src/app/hash/hash.page.spec.ts
+++ src/app/hash/hash.page.spec.ts
@@ -1,16 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AppService } from '../app.service';
+import { CryptoService } from '../crypto.service';
 
 import { HashPage } from './hash.page';
 
 describe('HashPage', () => {
   let component: HashPage;
   let fixture: ComponentFixture<HashPage>;
+  let appServiceSpy: jasmine.SpyObj<AppService>;
+  let cryptoServiceSpy: jasmine.SpyObj<AppService>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    appServiceSpy = jasmine.createSpyObj(AppService, ['copyOrShare']);
+    cryptoServiceSpy = jasmine.createSpyObj(CryptoService, ['hash']);
+
     TestBed.configureTestingModule({
       declarations: [ HashPage ],
-      imports: [ IonicModule.forRoot() ],
+      imports: [ IonicModule.forRoot(), ReactiveFormsModule ],
+      providers: [
+        { provide: AppService, useValue:  appServiceSpy },
+        { provide: CryptoService, useValue:  cryptoServiceSpy },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HashPage);
