ef7ba5eb41b5f53384086e70767acdd0d86660a6
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2021-02-24 23:08:14 +0100

    Fix setup encrypt page test

--- src/app/encrypt/encrypt.page.spec.ts
+++ src/app/encrypt/encrypt.page.spec.ts
@@ -1,16 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AppService } from '../app.service';
+import { CryptoService } from '../crypto.service';
 
 import { EncryptPage } from './encrypt.page';
 
 describe('EncryptPage', () => {
   let component: EncryptPage;
   let fixture: ComponentFixture<EncryptPage>;
+  let appServiceSpy: jasmine.SpyObj<AppService>;
+  let cryptoServiceSpy: jasmine.SpyObj<AppService>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    appServiceSpy = jasmine.createSpyObj(AppService, ['copyOrShare']);
+    cryptoServiceSpy = jasmine.createSpyObj(CryptoService, ['encrypt', 'decrypt']);
     TestBed.configureTestingModule({
       declarations: [ EncryptPage ],
-      imports: [ IonicModule.forRoot() ],
+      imports: [ IonicModule.forRoot(), ReactiveFormsModule ],
+      providers: [
+        { provide: AppService, useValue:  appServiceSpy },
+        { provide: CryptoService, useValue:  cryptoServiceSpy },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EncryptPage);
