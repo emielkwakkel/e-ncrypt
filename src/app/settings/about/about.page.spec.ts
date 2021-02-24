e889f49b0bd4cae3427b4038b6d226bd0758a52e
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2021-02-24 22:52:20 +0100

    Fix setup about page test

--- src/app/settings/about/about.page.spec.ts
+++ src/app/settings/about/about.page.spec.ts
@@ -1,16 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AboutPage } from './about.page';
 
 describe('AboutPage', () => {
   let component: AboutPage;
   let fixture: ComponentFixture<AboutPage>;
+  let browserSpy: jasmine.SpyObj<any>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    browserSpy = jasmine.createSpyObj('Browser', ['open']);
+    browserSpy.open.and.returnValue(true);
     TestBed.configureTestingModule({
       declarations: [ AboutPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        RouterTestingModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AboutPage);
